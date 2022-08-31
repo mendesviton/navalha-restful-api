9f19a1fc2aadd87258941b525db8c55d85b6b77e
Vitor Daniel Mendes Dos Santos <Vitor.Santos@proway.treina>
2022-08-31 13:20:09 -0300

    [feat] barber module

---
+++ navalha-backend/Controllers/BarberController.cs
@@ -0,0 +1,17 @@
+﻿using data.model;
+using data.repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers;
+
+namespace navalha_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarberController : GenericController<BarberModel, BarberRepository>
+    {
+        public BarberController() : base(new BarberRepository())
+        {
+        }
+    }
+}
