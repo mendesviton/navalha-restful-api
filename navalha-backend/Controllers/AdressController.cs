7bb5002cbddf3b62c573539ecf4f34cd9fdba10e
Guilherme Pedro Milani <Guilherme.Milani@proway.treina>
2022-08-31 11:35:10 -0300

    [feat]Adicionando Adress

---
+++ navalha-backend/Controllers/AdressController.cs
@@ -0,0 +1,16 @@
+﻿using data.model;
+using data.repository;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers;
+
+namespace navalha_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdressController: GenericController<AdressModel, AdressRepository>
+    {
+        public AdressController() : base(new AdressRepository())
+        {
+        }
+    }
+}
