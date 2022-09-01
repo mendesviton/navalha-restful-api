925ab829b4f2ff4883579f3d96229c40e6fb182e
Vitor Daniel <vitor6890@gmail.com>
2022-09-01 00:06:02 -0300

    [feat] little changes

--- navalha-backend/Controllers/ClientController.cs
+++ navalha-backend/Controllers/ClientController.cs
@@ -4,72 +4,22 @@ using DocumentFormat.OpenXml.Office2010.Excel;
 using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Controllers;
 
 namespace navalha_backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ClientController : ControllerBase
+    public class ClientController : GenericController<ClientModel, ClientRepository>
     {
         ClientRepository clientRepository;
-        
-        
-        public ClientController()
+
+
+        public ClientController() : base(new ClientRepository())
         {
-            clientRepository = new ClientRepository();
         }
 
-        [HttpGet]
-        public dynamic Get()
-        {
-            List<ClientModel> clientModelList = clientRepository.GetAll();
-            Dictionary<string, dynamic> returnMap = new Dictionary<string, dynamic>();
-
-            if (clientModelList.Count == 0)
-            {
-                returnMap.Add("result", "user not exists");
-            }
-            else
-            {
-                returnMap.Add("result", clientRepository.GetAll());
-            }
-            return returnMap;
-        }
-
-        [HttpPost]
-        public string Post(ClientModel model)
-        {
-
-            return clientRepository.Create(model);
-
-
-        }
-
-        [HttpDelete]
-        public string Delete(int id)
-        {
-            return clientRepository.Delete(id);
-        }
-
-        [HttpGet, Route("ClientById")]
-        public dynamic GetById(int id)
-        {
-            ClientModel clientModel = clientRepository.GetById(id);
-            Dictionary<string, dynamic> returnMap = new Dictionary<string, dynamic>();
-
-
-            if (clientModel is null)
-            {
-               returnMap.Add("result","user not exists");
-
-            }
-            else
-            {
-                returnMap.Add("result", clientModel);
-            }
-            return returnMap;
-
-        }
+       
 
 
 
